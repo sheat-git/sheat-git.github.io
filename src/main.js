d321ab36499eefd4518669c43960c98935a570c1
sheat-git <79030436+sheat-git@users.noreply.github.com>
2021-05-19 18:08:52 +0900

    Add files via upload

---
+++ src/main.js
@@ -0,0 +1,72 @@
+function getParam(name, url) {
+    if (!url) url = window.location.href;
+    name = name.replace(/[\[\]]/g, "\\$&");
+    var regex = new RegExp("[?&]" + name + "(=([^&#]*)|&|#|$)"),
+        results = regex.exec(url);
+    if (!results) return null;
+    if (!results[2]) return '';
+    return decodeURIComponent(results[2].replace(/\+/g, " "));
+}
+
+function load(dp){
+    const user = getParam('user');
+    console.log(user);
+    fetch(`src/user/${user}/import.js`).then(r=>{return r.text()}).then(t=>{
+        eval(t);
+        dp(l);
+    });
+}
+
+function display(l){
+    const teams = l.teams;
+    const teamNum = teams.length;
+    const scores = l.scores;
+    const left = l.left;
+    const dif = l.dif;
+    const win = l.win;
+    document.getElementById(`team-num-${teamNum}`).style.setProperty('display', 'flex');
+    document.querySelectorAll(`#team-num-${teamNum}`).forEach($container => {
+        if (teamNum === 2) {
+            $container.querySelectorAll('.score').forEach(($score, i) => {
+                $score.innerText = scores[i];
+            });
+            $container.querySelectorAll('.team-span').forEach(($team, i) => {
+                $team.innerText = teams[i];
+            });
+            $container.querySelectorAll('.score-dif').forEach(($dif, i) => {
+                $dif.innerText = dif;
+            });
+            $container.querySelectorAll('.win').forEach(($win, i) => {
+                $win.style.setProperty('display', win ? 'block' : 'none');
+            });
+            $container.querySelectorAll('.left-race').forEach(($race, i) => {
+                $race.innerText = `残レース:${left}`;
+            });
+        } else {
+            $container.querySelectorAll('.score').forEach(($score, i) => {
+                $score.innerText = scores[i];
+            });
+            $container.querySelectorAll('.team-span').forEach(($team, i) => {
+                $team.innerText = teams[i];
+            });
+            $container.querySelectorAll('.dif').forEach(($dif, i) => {
+                $dif.innerText = `+${scores[i] - scores[i+1]}`;
+            });
+            $container.querySelectorAll('.left-race').forEach(($race, i) => {
+                $race.innerText = `残${left}`;
+            });
+        }
+        $container.querySelectorAll('.team-span').forEach($span => {
+            var $parent = $span.parentNode;
+            var scaleX = 1;
+            var translateX = 0;
+            if ($span.offsetWidth > ($parent.offsetWidth - 20)) {
+                scaleX = ($parent.offsetWidth - 20) / $span.offsetWidth;
+                translateX = 10;
+            }
+            $span.style.setProperty('transform', `translateX(${translateX}px) scaleX(${scaleX})`);
+        });
+    });
+}
+
+load(display);
